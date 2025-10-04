2ed024e00c8c8e0adf4974c22e6955a98e1bee2a
Daniel Velásquez <141380631+DannyLaine158@users.noreply.github.com>
2025-10-03 18:57:07 -0600

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes.movies import movies_bp
+from database import get_db
 
 # Instancia de Flask
 app = Flask(__name__)
@@ -10,7 +11,16 @@ app.register_blueprint(movies_bp, url_prefix="/movies")
 # Endpoint root
 @app.route("/")
 def hello():
-    return {"Hola": "Este es un saludo"}
+    try:
+        conn = get_db()
+        cursor = conn.cursor()
+        # Comando de prueba
+        cursor.execute("SELECT NOW();")
+        result = cursor.fetchone() # Esperamos 1 fila
+        return { "time": str(result[0]) }
+    except Exception as e:
+        return { "error": str(e) }
+    # return {"Hola": "Este es un saludo"}
 
 # Punto de partida:
 if __name__ == "__main__":
